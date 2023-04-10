bd4383f0017d841c8aefbb8332108c019ba7f039
nexusbusim <ilov1112@gmail.com>
2023-04-10 14:44:42 +0900

    글 작성 및 메인화면에서 자신이 쓴 글만 미리보기 기능을 추가했습니다.
    글 작성 및 미리보기
    - 글 작성 추가
    - 미리보기 추가

---
+++ src/screen/main/MyCards.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { TailSpin } from 'react-loader-spinner';
+
+import Styles from './MyCards.module.css';
+import Card from '../../components/Card';
+
+const MyCards = () => {
+
+    const [cards, setCards] = useState([]);
+
+    const fetchCards = async () => {
+        const data = await (await fetch("https://software-engineering-3team-default-rtdb.firebaseio.com/cards.json", {method: 'GET'})).json();
+        const writings = []
+        for(const d in data) {
+            writings.push(data[d])
+        }
+        setCards(writings)
+    }
+
+    useEffect(
+        () => {
+            fetchCards();
+        }
+    , []);
+
+    if(cards.length === 0) {
+        return (
+            <div className={Styles.spinnerDiv}>
+                <TailSpin
+                    height="40"
+                    width="40"
+                    color="black"
+                    ariaLabel="tail-spin-loading"
+                    radius="1"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                />
+            </div>
+        )
+    }
+
+    return (
+        <div className={Styles.mainDiv}>
+            <div className={Styles.hashtagsDiv}>
+                <p className={Styles.hashtag}>#해시태그</p>
+            </div>
+            <div className={Styles.cardsDiv}>
+                {
+                    cards.map((item) => {
+                        return <Card key={item.writtenDate} card={item} style={{width: '280px', margin: '0px 30px 30px 0px'}}/>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MyCards;
