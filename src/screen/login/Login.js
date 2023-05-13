90cc1b280adc128037abd4898fdcc5281d497a09
nexusbusim <ilov1112@gmail.com>
2023-05-13 13:53:49 +0900

    쿠키 인식 개선

--- src/screen/login/Login.js
+++ src/screen/login/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
     useEffect(() => {
         setRefreshed(true);
         document.location.reload();
-    }, []);
+    }, [refreshed]);
 
     useEffect(() => {
         const interval = setInterval(
