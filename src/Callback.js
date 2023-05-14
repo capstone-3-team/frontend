336c9439742aabc32c3ddcbc2b0951a63e82e2e8
nexusbusim <ilov1112@gmail.com>
2023-05-14 17:02:27 +0900

    c

--- src/Callback.js
+++ src/Callback.js
@@ -8,6 +8,7 @@ const Callback = () => {
     const location = useLocation();
 
     const code = location.search.replace("?code=", "");
+    code.replace("&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+openid&authuser=0&prompt=none", "")
 
     return (
         <p>{code}</p>
